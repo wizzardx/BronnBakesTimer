622f152dda9d3fa1c0c7feec2566de1feda4fb81
david <wizzardx@gmail.com>
2023-12-14 10:19:16 +0200

    Update dependencies.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -59,7 +59,7 @@ dependencies {
     // Core AndroidX libraries
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
-    implementation("androidx.activity:activity-compose:1.8.1")
+    implementation("androidx.activity:activity-compose:1.8.2")
 
     // Jetpack Compose UI libraries
     implementation(platform("androidx.compose:compose-bom:2023.10.01"))
